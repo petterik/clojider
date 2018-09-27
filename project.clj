fe59afb7d4c554e8571bf61ceee552d1121b0dee
Petter Eriksson <ipetterik@gmail.com>
2018-09-27 16:20:12 -0700

    0.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojider "0.5.0"
+(defproject org.clojars.petterik/clojider "0.5.1-SNAPSHOT"
   :description "AWS Lambda powered, distributed load testing tool for Clojure"
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
